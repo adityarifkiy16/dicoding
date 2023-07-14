4041ae637e35c2c1d46be7b93f53c096758f6d2c
adityarifkiy16 <adityarifkistemba@gmail.com>
2023-07-14 14:22:45 +0700

    chain promise

---
+++ asynchronous/bayarKuliah.js
@@ -0,0 +1,46 @@
+const { resolve } = require("path");
+
+function withDrawMoney(amount) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (amount > 2000) {
+        reject(new Error("Not enough money to withdraw"));
+      }
+      resolve(amount);
+    }, 1000);
+  });
+}
+
+function bayarKuliah(money) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (money < 900) {
+        reject(new Error("Not enough money to pay!"));
+      } else {
+        resolve(`${money - 900} uang kembalian anda`);
+      }
+      resolve("payment successfull!");
+    }, 1000);
+  });
+}
+
+function cetakStruk() {
+  withDrawMoney(1900)
+    /*
+     * Dalam chaining promise setiap fungsi then harus mengembalikan promise baru
+     * untuk menjalankan proses chaining karena nilai yang dikembalikan tersebut akan dibawa ke fungsi then selanjutnya
+     */
+
+    /*
+     ? return 1900 lalu dimasukan kedalam argumen 'money'
+     */
+
+    /*
+     *return bayarKuliah(money)
+     */
+    .then((money) => bayarKuliah(money))
+    .then((result) => console.log(result))
+    .catch((err) => console.log(err.message));
+}
+
+cetakStruk();
